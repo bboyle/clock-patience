adf72193fbf09556c928a9d0bdaaf2405b2356ad
Ben Boyle <benjamins.boyle@gmail.com>
2013-06-01 01:35:01 +1000

    animation UI for dragging
    added 'active' state
    refactor dragging state
    set class rather than style properties in js

--- app/js/clockp.js
+++ app/js/clockp.js
@@ -1,6 +1,5 @@
 function handleDragStart( e ) {
-	this.style.opacity = '0.4';  // this / e.target is the source node.
-	this.style.cursor = '-moz-grabbing';
+	this.classList.add( 'dragging' );
 }
 
 var cards = document.querySelectorAll( '.card' );
