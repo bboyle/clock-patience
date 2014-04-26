b8a519b0509ba929b93cb37bfc97c629a4f030fb
Ben Boyle <benjamins.boyle@gmail.com>
2014-04-26 14:42:39 +1000

    regenerated app

--- app/js/clockp.js
+++ app/js/clockp.js
@@ -1,11 +1,11 @@
-/*! Clock patience - v0.0.5 - 2013-09-23
+/*! Clock patience - v0.0.6 - 2014-04-26
 * https://github.com/bboyle/clock-patience
-* Copyright (c) 2013 Ben Boyle; Licensed MIT */
+* Copyright (c) 2014 Ben Boyle; Licensed MIT */
 function handleDragStart() {
 	this.classList.add( 'dragging' );
 }
 
 var cards = document.querySelectorAll( '.card' );
-[].forEach.call( cards, function( card ) {
+[].forEach.call( cards, function ( card ) {
 	card.addEventListener( 'dragstart', handleDragStart, false );
-});
+} );
